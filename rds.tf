5905662d931cc3ebfddf1caccc6a4b1d7cc4501f
Conrad Bohm <conrad.bohm@hotmail.co.uk>
2024-01-21 16:37:39 +0000

    basic rds config details

--- rds.tf
+++ rds.tf
@@ -1,4 +1,12 @@
-resource "aws_db_instance" "cyberduck" {
+resource "aws_db_instance" "aws_rds" {
+    db_name = "cyberduck_rds"
+    username = "default"
+    manage_master_user_password = true
+    multi_az = true
+
     engine = "mysql"
     instance_class = "db.t4g.large"
+    allocated_storage = 50
+
+    skip_final_snapshot = true
 }
