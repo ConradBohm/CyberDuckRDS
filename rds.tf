a9d4e91247c269dd5b30b55d51b640afca6e677f
Conrad Bohm <conrad.bohm@hotmail.co.uk>
2024-01-21 16:45:19 +0000

    use correct resource

--- rds.tf
+++ rds.tf
@@ -9,7 +9,7 @@ resource "aws_db_instance" "aws_rds" {
     allocated_storage = 50
 
     vpc_security_group_ids = [aws_security_group.rds_sg.id]
-    db_subnet_group_name = aws_db_subnet_group.my_db_subnet_group.name
+    db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
 
 
     skip_final_snapshot = true
