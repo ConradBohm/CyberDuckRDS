89240e8366b8a9f7f4b0562893dddb44b7cc805b
Conrad Bohm <conrad.bohm@hotmail.co.uk>
2024-01-21 16:44:17 +0000

    add subnet group

--- rds.tf
+++ rds.tf
@@ -8,5 +8,14 @@ resource "aws_db_instance" "aws_rds" {
     instance_class = "db.t4g.large"
     allocated_storage = 50
 
+    vpc_security_group_ids = [aws_security_group.rds_sg.id]
+    db_subnet_group_name = aws_db_subnet_group.my_db_subnet_group.name
+
+
     skip_final_snapshot = true
+}
+
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name = "cyberduck_rds_subnet_group"
+  subnet_ids = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
 }
